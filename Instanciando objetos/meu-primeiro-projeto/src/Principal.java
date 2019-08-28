4cb4ab49cf3920697d03b51a3d66152fe422ae19
Fernando Dantas <fernandojrsud@hotmail.com>
2019-08-28 11:43:09 -0300

    Começando com java orientado a objetos

---
+++ Instanciando objetos/meu-primeiro-projeto/src/Principal.java
@@ -0,0 +1,12 @@
+
+public class Principal {
+
+	public static void main(String[] args) {		
+		Carro meuCarro;	//declaramos a variável	
+		meuCarro = new Carro(); //instaciamos a variável com um objeto do tipo Carro
+		
+		Carro seuCarro = new Carro();//Simplificando a instanciação do objeto Carro com variável diferente
+		
+	}
+	
+}
