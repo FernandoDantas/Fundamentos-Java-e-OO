729beddcef174aa479d7b4095fc4a7810aea4288
Fernando Dantas <fernandojrsud@hotmail.com>
2019-08-31 10:56:46 -0300

    O objeto this

---
+++ O objeto this/meu-primeiro-projeto/src/Carro.java
@@ -0,0 +1,16 @@
+
+public class Carro {
+
+	String fabricante;
+	String modelo;
+	String cor;
+	int anoDeFabricacao;
+	boolean biCombustivel;
+	
+	void alterarModelo(String modelo) {
+		if(modelo != null) {
+			this.modelo = modelo;
+		}
+	}
+	
+}
